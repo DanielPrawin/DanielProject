bec71a0d9bc0299e76e0c84c7aa0b7b473531e88
DanielPrawin <44159183+DanielPrawin@users.noreply.github.com>
2018-10-15 18:12:25 +0530

    Update launchFF.java

--- Selenium/src/Package_seleniumj/launchFF.java
+++ Selenium/src/Package_seleniumj/launchFF.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class launchFF {
 	  static WebDriver driver;
-public static void main(String[] ar) {
+public static void main(String[] args) {
 System.setProperty("webdriver.gecko.driver", "D:\\Selenium_Demp\\geckodriver.exe");
 driver=new FirefoxDriver();
 driver.get("http://google.co.in/");
